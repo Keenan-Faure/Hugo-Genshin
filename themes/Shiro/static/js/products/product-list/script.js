bd59f715dc17f4445a41b68b82d519c3da68e723
Keenan Faure <97687673+Keenan-Faure@users.noreply.github.com>
2022-10-07 11:20:18 +0200

    Updated card colors

--- themes/Shiro/static/js/products/product-list/script.js
+++ themes/Shiro/static/js/products/product-list/script.js
@@ -2,3 +2,52 @@ $(document).ready(()=>
 {
     $('.cover').addClass('blurry');
 });
+
+$(document).ready(()=>
+{
+    $('#mo').on('click', ()=>
+    {
+        document.getElementById('m').scrollIntoView(false);
+        document.getElementById('m').classList.add('clickFade');
+        setTimeout(()=>
+        {
+            document.getElementById('m').classList.remove('clickFade');
+        },1500);
+    });
+    $('#li').on('click', ()=>
+    {
+        document.getElementById('l').scrollIntoView(false);
+        document.getElementById('l').classList.add('clickFade');
+        setTimeout(()=>
+        {
+            document.getElementById('l').classList.remove('clickFade');
+        },1500);
+    });
+    $('#in').on('click', ()=>
+    {
+        document.getElementById('i').scrollIntoView(false);
+        document.getElementById('i').classList.add('clickFade');
+        setTimeout(()=>
+        {
+            document.getElementById('i').classList.remove('clickFade');
+        },1500);
+    });
+    $('#su').on('click', ()=>
+    {
+        document.getElementById('s').scrollIntoView(false);
+        document.getElementById('s').classList.add('clickFade');
+        setTimeout(()=>
+        {
+            document.getElementById('s').classList.remove('clickFade');
+        },1500);
+    });
+    $('#wo').on('click', ()=>
+    {
+        document.getElementById('w').scrollIntoView(false);
+        document.getElementById('w').classList.add('clickFade');
+        setTimeout(()=>
+        {
+            document.getElementById('w').classList.remove('clickFade');
+        },1500);
+    });
+});
